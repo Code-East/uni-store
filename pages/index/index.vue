07c67356c3f156a484e0cbb24e5b11998cb406d4
Code-East <2531122734@qq.com>
2022-10-03 16:12:29 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<navigator url="/pages/list/list">点击list</navigator>
+		<navigator url="/subpkg/news/news">点击news</navigator>
+		<button @click="choseImg">点击获取照片</button>
+		<image :src="imgSrc"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				imgSrc:''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			choseImg(){
+				uni.chooseImage({
+					count:1,
+					sizeType:['orginal','compressed'],
+					sourceType:['album','camera'],
+					success:(res)=> { //注意写成箭头函数 不然获取不到this
+						console.log(this);
+						this.imgSrc = res.tempFilePaths[0];
+						console.log(this.imgSrc);
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ subpkg/news/news.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		news中心
+		<text>hahaha</text>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+view{
+	background-color: red;
+	display: flex;
+	justify-content: space-between;
+	text{
+		color: saddlebrown;
+	}
+}
+</style>
